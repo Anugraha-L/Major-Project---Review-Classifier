4da8683d257dfff009ee89789548d4d9de747109
Anugraha-L <65602856+Anugraha-L@users.noreply.github.com>
2021-04-17 20:56:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ if st.button('Predict'):
   if op[0]==0:
     st.title('Negative Review')
   elif op[0]==1:
-    st.title('Somewhat Negative Review')
-  elif op[0]==2:
     st.title('Neutral Review')
-  elif op[0]==3:
-    st.title('Somewhat Positive Review')
   else:
     st.title('Positive Review')
